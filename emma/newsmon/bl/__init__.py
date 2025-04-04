4ac33514a14e0df24d0fb9fd2076a8d886d38239
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-04-04 10:47:11 +0200

    Adding weak classifiers.

--- emma/newsmon/bl/__init__.py
+++ emma/newsmon/bl/__init__.py
@@ -428,7 +428,7 @@ def bl_svm2(args):
     from cuml.feature_extraction.text import TfidfVectorizer
     from cuml.svm import SVC
 
-    tfidf = TfidfVectorizer(analyzer='word', max_features=10000)
+    tfidf = TfidfVectorizer(max_features=10000)
 
     train_texts = tfidf.fit_transform(train_texts)
     train_labels = labeler.vectorize(train_labels)
