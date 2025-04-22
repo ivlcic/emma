a2547d5cb336dd755f918a0d417676f219964cf5
Nikola Ivačič <nikola.ivacic@dropchop.com>
2025-04-22 18:41:16 +0200

    Adding visualizations and classifier improvements.

--- emma/newsmon/te/__init__.py
+++ emma/newsmon/te/__init__.py
@@ -365,8 +365,8 @@ def te_test(args) -> int:
 
     trainer = Trainer(
         model=model,
-        compute_metrics=compute_metrics,
-        preprocess_logits_for_metrics=preprocess_logits_for_metrics
+        #compute_metrics=compute_metrics,
+        #preprocess_logits_for_metrics=preprocess_logits_for_metrics
     )
     print(f'{len(datasets["test"])}')
     t1 = time.time()
